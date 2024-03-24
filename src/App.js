97e195c5dbcb3e32764313b66c391858ad209341
Pivindi Nimanga <nimanga-cs101901@stu.ac.lk>
2024-03-24 13:34:45 +0530

    edit4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './Components/LginSignup/LoginSingup';
 
 function App() {
   return (
     <div>
-      
+      <LoginSignup/>
       
     </div>
   );
--- src/Components/LginSignup/LoginSingup.jsx
+++ src/Components/LginSignup/LoginSingup.jsx
@@ -1,8 +1,36 @@
 import React from "react"
-const LoginSignup = () =>{
+import "./LoginSignup.css"
+import user_icon from "../Assets/icons8-person-24.png"
+import email_icon from "../Assets/icons8-email-30.png"
+import password_icon from "../Assets/icons8-password-26.png"
+const LoginSignup = () => {
     return(
-        <div>
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+            <div className="input">
+                <img src={user_icon} alt=""></img>
+                <input type="text"></input>
+            </div>
 
+            <div className="input">
+                <img src={email_icon} alt=""></img>
+                <input type="email"></input>
+            </div>
+
+            <div className="input">
+                <img src={password_icon} alt=""></img>
+                <input type="password"></input>
+            </div>
+            <div className="forgot">Forgot password<span>  Click here </span></div>
+            <div className="sumbit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
+            </div>
+            </div>
         </div>
     )
 }
